a9c4c92f35df5f45793606f411c2625901e79127
Pranjal Kumar <pranjalk449@gmail.com>
2024-10-22 12:00:39 +0530

    all files of my blog app

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width='50px'}) {
+  return (
+    <img src="logo1.png" 
+    alt="logo"
+    style={{ width }}/>
+  )
+}
+
+export default Logo
---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import AddPost  from "./AddPost";
+import AllPosts  from "./AllPosts";
+import EditPost from "./EditPost";
+import Login  from "./Login";
+import Signup from "./Signup";
+import Post from "./Post";
+import Home from "./Home";
+
+export {
+    AddPost,
+    AllPosts,
+    EditPost,
+    Post,
+    Login,
+    Signup,
+    Home
+}
+
