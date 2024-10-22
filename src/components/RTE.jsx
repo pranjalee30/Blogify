701c5e0bd7e9bed1fa472554d2eff1a0806b20b1
Pranjal Kumar <pranjalk449@gmail.com>
2024-10-22 23:49:41 +0530

    fix

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -3,7 +3,7 @@ import { Editor } from '@tinymce/tinymce-react';
 import { Controller } from 'react-hook-form';
 import conf from '../conf/conf';
 
-export default function RTE({ name, control, label, defaultValue = ""}) {
+export default function RTE({ name, control, label, defaultValue = "", isReadOnly=false}) {
   return (
     <div className='w-full'>
       {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
@@ -16,7 +16,6 @@ export default function RTE({ name, control, label, defaultValue = ""}) {
           <Editor
             apiKey={conf.tinymceApiKey}
             value={value}
-            disabled={false}
             init={{
               height: 500,
               menubar: true,
